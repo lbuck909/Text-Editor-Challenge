18cb4e7122e541983927bdc3d261453df60ba422
LaiSha Buck <laishabuck@gmail.com>
2023-07-20 16:10:24 -0400

    add text to db syntax added

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+// Version & db
+const jateDb = await openDB('jate', 1);
+// new transaction w/ privileges
+const tx = jateDb.transaction('jate', 'readwrite');
+// desired object store
+const store = tx.objectStore('jate');
+// store and pass content
+const request = store.put({ id: 1, value: content });
+// confirmation request
+const result = await request;
+  console.log('🤖 - data saved to the database', result);
+};
+
+
+
+
+
+
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
