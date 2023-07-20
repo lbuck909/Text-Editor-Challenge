cd42205dd9676c1c702cdc551fb8ce6193d08f5f
LaiSha Buck <laishabuck@gmail.com>
2023-07-19 20:47:38 -0400

    asset caching added

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,19 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// TODO: Implement asset caching (use notes from InjectManifest#19)
+registerRoute(({request}) => ['style', 'script', 'worker'].includes(request.destination), 
+  
+new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // one month
+    }), 
+  ],
+})
+);
