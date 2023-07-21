5403ed2a1395bb0b0ff53397fadce7e4ccc6eaa3
LaiSha Buck <laishabuck@gmail.com>
2023-07-21 18:17:16 -0400

    changes

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -12,8 +12,8 @@ module.exports = () => {
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
-      header: ".src/js/header.js",
-      editor: ".src/js/editor.js",
+      header: "./src/js/header.js",
+      editor: "./src/js/editor.js",
       database: "./src/js/database.js",
     },
     output: {
